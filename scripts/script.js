ee62ff0adfea01ea2a142b70420ce6c94e64fb94
GGGeorgiev20 <GGGeorgiev20@codingburgas.bg>
2022-03-13 02:09:44 +0200

    Add starting advanced functions

--- scripts/script.js
+++ scripts/script.js
@@ -6,27 +6,49 @@ fetch("./scripts/countryList.json")
 
 // Get info about a country
 
-let getCountry = () => prompt("Name a Country", "Germany");
+let getCountry = () => document.querySelector('input').value;;
 
 function getCountryInfo (country) {
-    let info = "You canceled the quiz.";
     for (let i in countries) {
         if (countries[i].name == country)
-            info = countries[i].info;
+            return countries[i].info;
     }
-    return info;
+    return "No such country.";
 }
 
-let printCountryInfo = () => alert(getCountryInfo(getCountry()));
-let printRandomCountryInfo = () => alert(getCountryInfo(getCountryByID(generateRandomID())));
+function getCountryImage (country) {
+    for (let i in countries) {
+        if (countries[i].name == country)
+            return countries[i].image;
+    }
+    return "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Question_mark_%28black%29.svg/800px-Question_mark_%28black%29.svg.png";
+}
+
+function displayCountry() {
+    printCountryInfo();
+    displayCountryImage();
+}
+
+function displayRandomCountry() {
+    id = generateRandomID();
+    printRandomCountryInfo(id);
+    displayRandomCountryImage(id);
+}
+
+let printCountryInfo = () => document.getElementById("countryText").innerHTML = getCountryInfo(getCountry());
+let printRandomCountryInfo = (id) => document.getElementById("countryText").innerHTML = getCountryInfo(getCountryByID(id));
+
+let displayCountryImage = () => document.getElementById("countryImage").src = getCountryImage(getCountry());
+let displayRandomCountryImage = (id) => document.getElementById("countryImage").src = getCountryImageByID(id);
 
 // Test info about a country
     
-// let generateRandomQuestion = () => Math.random(3);
+let generateRandomQuestion = () => Math.random(3);
 
 let generateRandomID = () => Math.floor(Math.random() * countries.length);
 
 let getCountryByID = (id) => countries[id].name;
+let getCountryImageByID = (id) => countries[id].image;
 
 let testCountryInfo = () => prompt(getCountryInfo(getCountryByID(generateRandomID())));
 
